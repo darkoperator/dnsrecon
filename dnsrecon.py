dc8c257271bacd97b9d0a8679b90cced7f8f50ec
Carlos Perez <carlos_perez@darkoperator.com>
2010-12-24 18:01:51 -0400

    Removed unused function

--- dnsrecon.py
+++ dnsrecon.py
@@ -132,21 +132,6 @@ class AppURLopener(urllib.FancyURLopener):
     version = 'Mozilla/5.0 (compatible; Googlebot/2.1; + http://www.google.com/bot.html)'
 
 
-
-def clean_str(out_string):
-    # from http://boodebr.org/main/python/all-about-python-and-unicode#UNI_XML
-    RE_XML_ILLEGAL = u'([\u0000-\u0008\u000b-\u000c\u000e-\u001f\ufffe-\uffff])' + \
-                     u'|' + \
-                     u'([%s-%s][^%s-%s])|([^%s-%s][%s-%s])|([%s-%s]$)|(^[%s-%s])' % \
-                      (unichr(0xd800),unichr(0xdbff),unichr(0xdc00),unichr(0xdfff),
-                       unichr(0xd800),unichr(0xdbff),unichr(0xdc00),unichr(0xdfff),
-                       unichr(0xd800),unichr(0xdbff),unichr(0xdc00),unichr(0xdfff))
-    regex = re.compile(RE_XML_ILLEGAL)
-
-    for match in regex.finditer(out_string):
-        out_string = out_string[:match.start()] + " " + out_string[match.end():]
-    return out_string
-
 def unique(seq, idfun=repr):
     """
     Function to remove duplicates in an array. Returns array with duplicates
