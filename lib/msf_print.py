2552958e964a0f51069193a2a3f64d37a764fa2e
Carlos Perez <carlos_perez@darkoperator.com>
2012-01-15 16:22:39 -0400

    added print_debug, empty message if none given and added GPL and Copiright header

--- lib/msf_print.py
+++ lib/msf_print.py
@@ -1,15 +1,34 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-def print_status(message):
+#    Copyright (C) 2012  Carlos Perez
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; Applies version 2 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+def print_status(message=""):
     print("\033[1;34m[*]\033[1;m {0}".format(message))
 
-def print_good(message):
+def print_good(message=""):
     print("\033[1;32m[*]\033[1;m {0}".format(message))
 
-def print_error(message):
+def print_error(message=""):
     print("\033[1;31m[-]\033[1;m {0}".format(message))
     
-def print_line(message):
+def print_debug(message=""):
+    print("\033[1;31m[!]\033[1;m {0}".format(message))
+    
+def print_line(message=""):
     print("{0}".format(message))
           
 
