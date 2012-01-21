29d5cf9abc1390393a0f4fa246b0969339febc8c
Carlos Perez <carlos_perez@darkoperator.com>
2012-01-21 14:44:28 -0400

    Uses nokogiri and a parses the XML file much faster now, also tab completion for path to the XML file is added.

--- msf_plugin/dnsr_import.rb
+++ msf_plugin/dnsr_import.rb
@@ -1,15 +1,15 @@
 module Msf
 
     class Plugin::PostCommand < Msf::Plugin
-	
+
         class MultiCommand
             include Msf::Ui::Console::CommandDispatcher
-		
+
             # Set name for command dispatcher
             def name
                 "DNSR_Import"
             end
-		
+
             # Define Commands
             def commands
                 {
@@ -25,82 +25,85 @@ module Msf
             def report_service(name, proto, port, target)
                 print_good("Importing service #{name} for host #{target}")
                 framework.db.report_service({
-                        :host=> target,
-                        :name => name,
-                        :port => port,
+                        :host  => target,
+                        :name  => name,
+                        :port  => port,
                         :proto => proto
                     })
             end
 
             def parse_records(xm_file)
-                require "rexml/document"
-                file = File.new(xm_file)
-                doc = REXML::Document.new(file)
-                doc.elements.each("records/record") do |e|
-                    if e.elements['type'].text.strip =~ /PTR|^[A]$|AAAA/
-                        report_host(e.elements['address'].text.strip)
-                        
-                        # Since several hosts can be tied to one IP, specially those that are vhosts
-                        # or are behind a balancer the names are saved as notes.
-                        framework.db.report_note({
-                                :host => e.elements['address'].text.strip,
-                                :type => "dns.info",
-                                :data =>"A #{e.elements['name'].text.strip}",
-                            })
+                require "nokogiri"
+                reader = Nokogiri::XML::Reader(IO.read(xm_file))
+				reader.each do |node|
+					if node.name == 'record'
+						case node.attribute('type')
 
-                    elsif e.elements['type'].text.strip =~ /NS/
-                        report_host(e.elements['address'].text.strip)
-                        report_service("dns", "udp", 53, e.elements['address'].text.strip)
-                        framework.db.report_note({
-                                :host => e.elements['address'].text.strip,
-                                :port => 53,
-                                :proto => "udp",
-                                :ntype => "dns.info",
-                                :data =>"NS #{e.elements['target'].text.strip} "
-                            })
-         
-                    elsif e.elements['type'].text.strip =~ /SOA/
-                        report_host(e.elements['address'].text.strip)
-                        report_service("dns", "udp", 53, e.elements['address'].text.strip)
-                        framework.db.report_note({
-                                :host => e.elements['address'].text.strip,
-                                :port => 53,
-                                :proto => "udp",
-                                :type => "dns.info",
-                                :data =>"NS #{e.elements['mname'].text.strip} "
-                            })
+						when /PTR|^[A]$|AAAA/
+							report_host(node.attribute('address'))
 
-                    elsif e.elements['type'].text.strip =~ /MX/
-                        report_host(e.elements['address'].text.strip)
-                        report_service("smtp", "tcp", 25, e.elements['address'].text.strip)
-                        framework.db.report_note({
-                                :host => e.elements['address'].text.strip,
-                                :port => 25,
-                                :proto => "tcp",
-                                :type => "dns.info",
-                                :data =>"MX #{e.elements['exchange'].text.strip} "
-                            })
+							# Since several hosts can be tied to one IP, specially those that are vhosts
+							# or are behind a balancer the names are saved as notes.
+							framework.db.report_note({
+									:host  => node.attribute('address'),
+									:ntype => "dns.info",
+									:data  => "A #{node.attribute('name')}",
+									:update => :unique_data
+								})
 
-                    elsif e.elements['type'].text.strip =~ /SRV/
-                        srv_info = []
-                        srv_info = e.elements['name'].text.strip.scan(/^_(\S*)\._(tcp|udp|tls)\./)[0]
-                        port = e.elements['port'].text.strip.to_i
-                        if srv_info[1] == "tls"
-                            proto = "tcp"
-                        else
-                            proto = srv_info[1]
-                        end
-                        report_host(e.elements['address'].text.strip)
-                        report_service(srv_info[0], proto, port, e.elements['address'].text.strip)
-                        framework.db.report_note({
-                                :host => e.elements['address'].text.strip,
-                                :port => port,
-                                :proto => proto,
-                                :type => "dns.info",
-                                :data =>"SRV #{e.elements['target'].text.strip} "
-                            })
-     
-                    end
+						 when /NS/
+							report_host(node.attribute('address'))
+							report_service("dns", "udp", 53, node.attribute('address'))
+							framework.db.report_note({
+									:host  => node.attribute('address'),
+									:port  => 53,
+									:proto => "udp",
+									:ntype => "dns.info",
+									:data  => "NS #{node.attribute('target')}"
+								})
+
+						when /SOA/
+							report_host(node.attribute('address'))
+							report_service("dns", "udp", 53, node.attribute('address'))
+							framework.db.report_note({
+									:host  => node.attribute('address'),
+									:port  => 53,
+									:proto => "udp",
+									:type  => "dns.info",
+									:data  => "NS #{node.attribute('mname')}"
+								})
+
+						when /MX/
+							report_host(node.attribute('address'))
+							report_service("smtp", "tcp", 25, node.attribute('address'))
+							framework.db.report_note({
+									:host  => node.attribute('address'),
+									:port  => 25,
+									:proto => "tcp",
+									:type  => "dns.info",
+									:data  => "MX #{node.attribute('exchange')}"
+								})
+
+						when /SRV/
+							srv_info = []
+							srv_info = node.attribute('name').strip.scan(/^_(\S*)\._(tcp|udp|tls)\./)[0]
+							port = node.attribute('port').strip.to_i
+							if srv_info[1] == "tls"
+								proto = "tcp"
+							else
+								proto = srv_info[1]
+							end
+							report_host(node.attribute('address'))
+							report_service(srv_info[0], proto, port, node.attribute('address'))
+							framework.db.report_note({
+									:host  => node.attribute('address'),
+									:port  => port,
+									:proto => proto,
+									:type  => "dns.info",
+									:data  => "SRV #{node.attribute('target')}"
+								})
+						end
+					end
                 end
             end
             # Multi shell command
@@ -110,16 +113,16 @@ module Msf
                     "-f"   => [ true,	"XML file to import."],
                     "-h"   => [ false,  "Command Help"]
                 )
-			
+
                 # set variables for options
                 xml_file = ""
-			
+
                 # Parse options
                 opts.parse(args) do |opt, idx, val|
                     case opt
 					when "-f"
 						xml_file = val
-						
+
 					when "-h"
 						print_line(opts.usage)
 						return
@@ -132,8 +135,12 @@ module Msf
 				else
                         parse_records(xml_file)
                 end
-				
+
             end
+
+			def cmd_import_dnsrecon_xml_tabs(str, words)
+				tab_complete_filenames(str, words)
+			end
         end
 
         def initialize(framework, opts)
@@ -141,7 +148,7 @@ module Msf
             add_console_dispatcher(MultiCommand)
             print_status("dnsr_import plugin loaded.")
         end
-	
+
         def cleanup
             remove_console_dispatcher("DNSR_Import")
         end
@@ -149,7 +156,7 @@ module Msf
         def name
             "dnsr_import"
         end
-	
+
         def desc
             "Allows to import DNSRecon output XML files."
         end
