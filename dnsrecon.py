1d35fcc134b3485d0d31d3961e19ed9462e6e4c2
Carlos Perez <carlos_perez@darkoperator.com>
2013-05-19 11:39:56 -0400

    bump version

--- dnsrecon.py
+++ dnsrecon.py
@@ -18,7 +18,7 @@
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-__version__ = '0.8.3'
+__version__ = '0.8.4'
 __author__ = 'Carlos Perez, Carlos_Perez@darkoperator.com'
 
 __doc__ = """
