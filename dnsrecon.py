7d71872bcc7b40fdc62bcac354e04b1ab0404c72
Carlos Perez <carlos_perez@darkoperator.com>
2014-01-05 11:50:34 -0400

    Update the copyright for the new year

--- dnsrecon.py
+++ dnsrecon.py
@@ -3,7 +3,7 @@
 
 #    DNSRecon
 #
-#    Copyright (C) 2012  Carlos Perez
+#    Copyright (C) 2014  Carlos Perez
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
