f01e6ccd9930016a8835810d3e8d3d0b0e3383ab
L1ghtn1ng <townsend891@hotmail.com>
2020-04-14 00:54:09 +0100

    Add py3.6 to the classifiers in setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
