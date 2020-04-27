ddfa0fc56f548c623a6a92b742e98de1d4103cd3
Lee Baird <leebaird@gmail.com>
2020-04-27 19:55:07 -0400

    Fixed formatting for SRV records.

--- dnsrecon.py
+++ dnsrecon.py
@@ -359,7 +359,7 @@ def brute_srv(res, domain, verbose=False, thread_num=None):
                                          "target": rcd[2],
                                          "address": rcd[3],
                                          "port": rcd[4]})
-                print_good({"type": rcd[0], "name": rcd[1], "target": rcd[2], "address": rcd[3], "port": rcd[4]})
+                print_good(f"     {rcd[0]} {rcd[1]} {rcd[2]} {rcd[3]} {rcd[4]}")
     else:
         print_error(f"No SRV Records Found for {domain}")
 
