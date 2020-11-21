f2db0d25583ef6c3fa3597f4b3d5e12a3f64b8ba
SecurityShrimp <f8al.err0r@gmail.com>
2020-11-20 20:28:16 -0600

    Update __init__.py

--- dnsrecon/__init__.py
+++ dnsrecon/__init__.py
@@ -5,9 +5,8 @@ dnsrecon python module adaptation
 '''
 
 from .lib.crtenum import scrape_crtsh
-from .lib.bingenum import scrape_bing
-from .lib.yandexenum import scrape_yandex
+from .lib.yandexenum import *
+from .lib.bingenum import *
+from .lib.dnshelper import DnsHelper
 from .dnsrecon import *
-from .msf_print import *
-from .whois import *
-from .lib.output import *
+from .lib.msf_print import print_debug, print_status, print_error, print_good, print_line
