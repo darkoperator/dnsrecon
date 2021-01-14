3eadcb305a5065fd4d6d5f35a8c328cc370d2061
Filippo <filippolauria@users.noreply.github.com>
2021-01-14 11:19:21 +0100

    little "optimization" when printing bind version
    Swapped the two lines with the aim of reusing version in the print_status, instead of accessing twice response.answer[0].items[0].strings[0]

--- dnsrecon.py
+++ dnsrecon.py
@@ -844,8 +844,9 @@ def check_bindversion(res, ns_server, timeout):
         try:
             response = res.query(request, ns_server, timeout=timeout, one_rr_per_rrset=True)
             if len(response.answer) > 0 and 'items' in response.answer[0]:
-                print_status(f"\t Bind Version for {ns_server} {response.answer[0].items[0].strings[0]}")
                 version = response.answer[0].items[0].strings[0]
+                print_status(f"\t Bind Version for {ns_server} {version}")
+                
         except (dns.resolver.NXDOMAIN, dns.exception.Timeout, dns.resolver.NoAnswer, socket.error,
                 dns.query.BadResponse):
             pass
