b7a4091c97a18fc6b27831cc2fd4afe148fe90c9
Hyatche <33911325+Hyatche@users.noreply.github.com>
2021-03-11 17:55:53 +0100

    fix make_csv
    On make_csv function the representation of data[i] is a list of one dictionary we want to exploit this dictionary.
    Fix one part of  #173 issue CSV and sqlite output aren't written to disk.

--- dnsrecon.py
+++ dnsrecon.py
@@ -685,7 +685,10 @@ def create_db(db):
 
 def make_csv(data):
     csv_data = "Type,Name,Address,Target,Port,String\n"
-    for record in data:
+    for record_tmp in data:
+        # the representation of data[i] is a list of one dictionary
+        # we want to exploit this dictionary
+        record = record_tmp[0]
         # make sure that we are working with a dictionary.
         if not isinstance(record, dict):
             continue
