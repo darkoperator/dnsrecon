5ad99a5335bf041847d284cb4bb8b66304f30382
J.Townsend <L1ghtn1ng@users.noreply.github.com>
2022-06-07 18:10:24 +0100

    update setup.py for new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dnsrecon",
-    version="1.1.0",
+    version="1.1.1",
     author="Carlos Perez",
     author_email="carlos_perez@darkoperator.com",
     description="DNS Enumeration Script",
