32fc2656f40b7629356e1dadcb671e81d6fde4ae
L1ghtn1ng <townsend891@hotmail.com>
2024-10-18 19:56:54 +0100

    Update version to 1.3.0
    Increased the version number from 1.2.0 to 1.3.0, this change reflects recent updates and improvements to the project.

--- dnsrecon/cli.py
+++ dnsrecon/cli.py
@@ -16,7 +16,7 @@
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-__version__ = '1.2.0'
+__version__ = '1.3.0'
 __author__ = 'Carlos Perez, Carlos_Perez@darkoperator.com'
 
 __doc__ = """
